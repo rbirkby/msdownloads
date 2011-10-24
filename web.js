d8382f132274f0737b3f44d3427976467340ebc1
Richard Birkby <rbirkby@gmail.com>
2011-10-24 07:49:07 +0100

    Use request to make http requests directly instead of using jsdom to try to fix an Heroku issue

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var http = require("http");
 var jsdom = require("jsdom");
 var jstoxml = require("jstoxml");
+var request = require('request');
 var express = require("express");
 
 console.log("starting msdownloads");
@@ -27,48 +28,54 @@ function updateContent() {
 	{title: "Microsoft Download Center"},
 	{link: "http://www.microsoft.com/downloads/"},
 	{description: "The fifty latest downloads from the Microsoft Download Center. (For personal and non-commercial use only.)"},
-	{lastBuildDate: function() {return (new Date()).toGMTString(); }}
+	{lastBuildDate: (new Date()).toGMTString() }
       ]	
     }
   };  
   
   if(!content) content = poweredBy + jstoxml.toXML(feed, false, '');
 
-  jsdom.env("http://www.microsoft.com/download/en/search.aspx?q=t%2a&p=0&r=50&t=&s=availabledate~Descending", [
-    'http://code.jquery.com/jquery.min.js'
-  ],
-  function(errors, window) {
-    if(errors) {
-      console.log(errors);
-    } else {    
-      console.log("retrieved content");
-      var $ = window.jQuery;
-
-      $(function () {
-	var items = $("td.descTD");
-	console.log("got " + items.length + " items");
-
-	items.each(function(index, item) {
-	  feed._content.channel.push({
-	    item: {
-	      title:encode($("div.link a", item).text()),
-	      link:"http://www.microsoft.com" + $("div.link a", item).attr("href") + "#tm",
-	      description:encode($("div.description", item).text())
-	    }
-	  });
-	});
-	content = poweredBy + jstoxml.toXML(feed, false, '');
-	console.log("rss output updated");
-      
-	setTimeout(function() {window.close();}, 500);
-      });
+  request({ uri:"http://www.microsoft.com/download/en/search.aspx?q=t%2a&p=0&r=50&t=&s=availabledate~Descending" }, function (error, response, body) {
+    if (error && response.statusCode !== 200) {
+      console.log('Error ' + error);
     }
+
+    jsdom.env({
+      html: body,
+      scripts:[ 
+	'http://code.jquery.com/jquery.min.js'
+      ]},
+      function(errors, window) {
+	if(errors) {
+	  console.log(errors);
+	} else {    
+	  console.log("retrieved content");
+	  var $ = window.jQuery;
+
+	  $(function () {
+	    var items = $("td.descTD");
+	    console.log("got " + items.length + " items");
+
+	    items.each(function(index, item) {
+	      feed._content.channel.push({
+		item: {
+		  title:encode($("div.link a", item).text()),
+		  link:"http://www.microsoft.com" + $("div.link a", item).attr("href") + "#tm",
+		  description:encode($("div.description", item).text())
+		}
+	      });
+	    });
+	    content = poweredBy + jstoxml.toXML(feed, false, '');
+	    console.log("rss output updated");
+	  
+	    setTimeout(function() {window.close();}, 500);
+	  });
+	}
+      });
   });
 }
 
-// Heroku appears to require a delay before we make a 
-// net connection
-setTimeout(updateContent, 5000);
+updateContent(); 
 setInterval(updateContent, 60000 * 5);
 
 var app = express.createServer(express.logger());
